eeae6bfc1b95479d4bb43680f5f05796e494a826
Jakub Kadlubiec <jakub.kadlubiec@skyscanner.net>
2016-08-18 15:41:55 +0200

    Add retry policy tests

---
+++ tests/test_retry_policy.py
@@ -0,0 +1,45 @@
+from failsafe.failsafe import Context
+from failsafe.retry_policy import RetryPolicy
+
+
+class TestRetryPolicy:
+
+    def test_should_retry_when_attempts_is_less_then_limit(self):
+        retry_policy = RetryPolicy(allowed_retries=3)
+
+        context = Context()
+        context.attempts = 3
+
+        assert retry_policy.should_retry(context) is True
+
+    def test_should_not_retry_when_there_were_too_many_attempts(self):
+        retry_policy = RetryPolicy(allowed_retries=3)
+
+        context = Context()
+        context.attempts = 4
+
+        assert retry_policy.should_retry(context) is False
+
+    def test_should_not_retry_when_exception_is_not_retriable(self):
+        retry_policy = RetryPolicy(allowed_retries=3, retriable_exception=BufferError)
+
+        context = Context()
+        context.attempts = 3
+
+        assert retry_policy.should_retry(context, ArithmeticError()) is False
+
+    def test_should_retry_when_exception_is_retriable(self):
+        retry_policy = RetryPolicy(allowed_retries=3, retriable_exception=BufferError)
+
+        context = Context()
+        context.attempts = 3
+
+        assert retry_policy.should_retry(context, BufferError()) is True
+
+    def test_should_not_retry_when_exception_is_retriable_but_there_were_too_many_attempts(self):
+        retry_policy = RetryPolicy(allowed_retries=3, retriable_exception=BufferError)
+
+        context = Context()
+        context.attempts = 4
+
+        assert retry_policy.should_retry(context, BufferError()) is False
