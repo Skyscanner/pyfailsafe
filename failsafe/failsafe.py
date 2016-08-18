abd377703174a5631c05314caebfec59338d3883
Jakub Kadlubiec <jakub.kadlubiec@skyscanner.net>
2016-08-18 14:42:51 +0200

    Move context to the method scope

--- failsafe/failsafe.py
+++ failsafe/failsafe.py
@@ -5,16 +5,16 @@ from failsafe.retry_policy import RetryPolicy
 class Failsafe:
 
     def __init__(self, retry_policy=None, circuit_breaker=None):
-        self.context = Context()
         self.retry_policy = retry_policy or RetryPolicy(0)
         self.circuit_breaker = circuit_breaker or AlwaysClosedCircuitBreaker()
 
     async def run(self, callable):
         callables = [(callable, self.circuit_breaker)]
+
         while callables:
             callable, circuit_breaker = callables.pop(0)
             retry = True
-            self.context = Context()
+            context = Context()
 
             while retry:
                 if circuit_breaker and not circuit_breaker.allows_execution():
@@ -25,15 +25,15 @@ class Failsafe:
                         raise CircuitOpen()
 
                 try:
-                    self.context.attempts += 1
+                    context.attempts += 1
                     result = await callable()
                     if circuit_breaker:
                         circuit_breaker.record_success()
                     return result
 
                 except Exception as e:
-                    self.context.errors += 1
-                    retry = self.retry_policy.should_retry(self.context, e) if self.retry_policy else False
+                    context.errors += 1
+                    retry = self.retry_policy.should_retry(context, e) if self.retry_policy else False
                     if circuit_breaker:
                         circuit_breaker.record_failure()
 
--- tests/test_failsafe.py
+++ tests/test_failsafe.py
@@ -6,74 +6,95 @@ import pytest
 from failsafe import RetryPolicy, Failsafe, CircuitOpen, CircuitBreaker, RetriesExhausted
 
 
+loop = asyncio.get_event_loop()
+
+
+def create_succeeding_operation():
+    async def operation():
+        operation.called += 1
+
+    operation.called = 0
+    return operation
+
+
+def create_failing_operation():
+    async def operation():
+        operation.called += 1
+        raise SomeRetriableException()
+
+    operation.called = 0
+    return operation
+
+
 class SomeRetriableException(Exception):
     pass
 
 
-async def successful_operation():
-    return 1
-
-
-async def failing_operation():
-    raise SomeRetriableException()
-
-
-loop = asyncio.get_event_loop()
-
-
 class TestFailSafe(unittest.TestCase):
+
     def test_no_retry(self):
+        succeeding_operation = create_succeeding_operation()
         loop.run_until_complete(
-            Failsafe().run(successful_operation)
+            Failsafe().run(succeeding_operation)
         )
 
     def test_basic_retry(self):
+        succeeding_operation = create_succeeding_operation()
         policy = RetryPolicy()
         loop.run_until_complete(
-            Failsafe(retry_policy=policy).run(successful_operation)
+            Failsafe(retry_policy=policy).run(succeeding_operation)
         )
 
     def test_retry_once(self):
+        failing_operation = create_failing_operation()
         expected_attempts = 2
         retries = 1
         policy = RetryPolicy(retries)
         failsafe = Failsafe(retry_policy=policy)
-        assert failsafe.context.attempts == 0
+
+        assert failing_operation.called == 0
+
         with pytest.raises(RetriesExhausted):
             loop.run_until_complete(
                 failsafe.run(failing_operation)
             )
 
-        assert failsafe.context.attempts == expected_attempts
+        assert failing_operation.called == expected_attempts
 
     def test_retry_four_times(self):
+        failing_operation = create_failing_operation()
         expected_attempts = 5
         retries = 4
         policy = RetryPolicy(retries)
         failsafe = Failsafe(retry_policy=policy)
-        assert failsafe.context.attempts == 0
+
+        assert failing_operation.called == 0
 
         with pytest.raises(RetriesExhausted):
             loop.run_until_complete(
                 failsafe.run(failing_operation)
             )
 
-        assert failsafe.context.attempts == expected_attempts
+        assert failing_operation.called == expected_attempts
 
     def test_retry_on_custom_exception(self):
+        failing_operation = create_failing_operation()
         retries = 3
         policy = RetryPolicy(retries, SomeRetriableException)
         failsafe = Failsafe(retry_policy=policy)
-        assert failsafe.context.attempts == 0
+
+        assert failing_operation.called == 0
 
         with pytest.raises(RetriesExhausted):
             loop.run_until_complete(
                 failsafe.run(failing_operation)
             )
 
-        assert failsafe.context.attempts == retries + 1
+        assert failing_operation.called == retries + 1
 
     def test_circuit_breaker(self):
+        failing_operation = create_failing_operation()
+
         with pytest.raises(CircuitOpen):
             policy = RetryPolicy(5, SomeRetriableException)
             circuit_breaker = CircuitBreaker(maximum_failures=2)
@@ -81,3 +102,5 @@ class TestFailSafe(unittest.TestCase):
                 Failsafe(retry_policy=policy, circuit_breaker=circuit_breaker)
                 .run(failing_operation)
             )
+
+        assert failing_operation.called == 2
