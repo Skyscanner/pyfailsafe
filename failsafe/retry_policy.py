94ae8d3c9f736bf340554970a6ef40b51cecde91
Carlos Perez <carlos.perez@skyscanner.net>
2016-08-18 16:33:32 +0200

    Retry policy supports multiple execeptions

--- failsafe/retry_policy.py
+++ failsafe/retry_policy.py
@@ -1,11 +1,12 @@
 class RetryPolicy:
 
-    def __init__(self, allowed_retries=3, retriable_exception=None):
+    def __init__(self, allowed_retries=3, retriable_exceptions=None):
         self.allowed_retries = allowed_retries
-        self.retriable_exception = retriable_exception
+        self.retriable_exceptions = retriable_exceptions or []
 
     def should_retry(self, context, exception=None):
         return context.attempts <= self.allowed_retries and self._is_expected_exception(exception)
 
     def _is_expected_exception(self, exception):
-        return isinstance(exception, self.retriable_exception) if self.retriable_exception else True
+        return any(isinstance(exception, e) for e in self.retriable_exceptions) if self.retriable_exceptions else True
+
--- tests/test_retry_policy.py
+++ tests/test_retry_policy.py
@@ -21,7 +21,7 @@ class TestRetryPolicy:
         assert retry_policy.should_retry(context) is False
 
     def test_should_not_retry_when_exception_is_not_retriable(self):
-        retry_policy = RetryPolicy(allowed_retries=3, retriable_exception=BufferError)
+        retry_policy = RetryPolicy(allowed_retries=3, retriable_exceptions=[BufferError])
 
         context = Context()
         context.attempts = 3
@@ -29,7 +29,7 @@ class TestRetryPolicy:
         assert retry_policy.should_retry(context, ArithmeticError()) is False
 
     def test_should_retry_when_exception_is_retriable(self):
-        retry_policy = RetryPolicy(allowed_retries=3, retriable_exception=BufferError)
+        retry_policy = RetryPolicy(allowed_retries=3, retriable_exceptions=[BufferError])
 
         context = Context()
         context.attempts = 3
@@ -37,9 +37,18 @@ class TestRetryPolicy:
         assert retry_policy.should_retry(context, BufferError()) is True
 
     def test_should_not_retry_when_exception_is_retriable_but_there_were_too_many_attempts(self):
-        retry_policy = RetryPolicy(allowed_retries=3, retriable_exception=BufferError)
+        retry_policy = RetryPolicy(allowed_retries=3, retriable_exceptions=[BufferError])
 
         context = Context()
         context.attempts = 4
 
         assert retry_policy.should_retry(context, BufferError()) is False
+
+    def test_should_retry_with_more_that_one_exception_type(self):
+        retry_policy = RetryPolicy(allowed_retries=3, retriable_exceptions=[BufferError, ValueError])
+
+        context = Context()
+        context.attempts = 3
+
+        assert retry_policy.should_retry(context, BufferError()) is True
+        assert retry_policy.should_retry(context, ValueError()) is True
