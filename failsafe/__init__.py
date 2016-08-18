3f0fe40f74386a57a593c78650f7a57607912302
Jakub Kadlubiec <jakub.kadlubiec@skyscanner.net>
2016-08-18 15:34:15 +0200

    Expose FailsafeError

--- failsafe/__init__.py
+++ failsafe/__init__.py
@@ -1,3 +1,3 @@
-from .failsafe import Failsafe, CircuitOpen, RetriesExhausted  # noqa
+from .failsafe import Failsafe, FailsafeError, CircuitOpen, RetriesExhausted  # noqa
 from .circuit_breaker import CircuitBreaker  # noqa
 from .retry_policy import RetryPolicy  # noqa
