997bbddac7e39e6c515e5106273adc74b954c1a9
Jakub Kadlubiec <jakub.kadlubiec@skyscanner.net>
2016-08-18 15:00:14 +0200

    Rename exception parameter in RetryPolicy + small cleanup

--- failsafe/retry_policy.py
+++ failsafe/retry_policy.py
@@ -1,9 +1,11 @@
 class RetryPolicy:
 
-    def __init__(self, retries=0, exception=None):
-        self.retries = retries
-        self.exception = exception
+    def __init__(self, allowed_retries=3, retriable_exception=None):
+        self.allowed_retries = allowed_retries
+        self.retriable_exception = retriable_exception
 
     def should_retry(self, context, exception=None):
-        is_expected_exception = isinstance(exception, self.exception) if self.exception else True
-        return context.attempts <= self.retries and is_expected_exception
+        return context.attempts <= self.allowed_retries and self._is_expected_exception(exception)
+
+    def _is_expected_exception(self, exception):
+        return isinstance(exception, self.retriable_exception) if self.retriable_exception else True
