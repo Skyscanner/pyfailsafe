a68a3e397068ce61b3b2d1f886f513f3d6774e94
Jakub Kadlubiec <jakub.kadlubiec@skyscanner.net>
2016-09-21 13:39:14 +0100

    Remove support for running tests with setup.py - it didn't work on travis

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
-from pip.req import parse_requirements
 from setuptools import setup, find_packages
 import failsafe
 
-
-def get_requirements(file):
-    requirements = parse_requirements(file, session=False)
-    return [str(ir.req) for ir in requirements if not None]
 
 setup(
     name="pyfailsafe",
@@ -17,7 +12,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     platforms="any",
-    install_requires=[],
-    setup_requires=["pytest-runner"],
-    tests_require=get_requirements("requirements_test.txt")
+    install_requires=[]
 )
