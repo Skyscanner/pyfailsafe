01ef595089309c5dd1ee65cdf4cd2ac01b0cb67f
Flavio Curella <flavio.curella@gmail.com>
2018-03-27 15:11:06 -0500

    change jitter algorithm

--- failsafe/retry_policy.py
+++ failsafe/retry_policy.py
@@ -32,7 +32,7 @@ class Backoff:
         max_delay = self.max_delay.total_seconds()
 
         if self.jitter is True:
-            duration += random.random()
+            duration *= random.random()
 
         if duration > max_delay:
             return max_delay
